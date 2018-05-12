25ee6be558be0662103075d84f81e11173100a53
Magdalena Gresk <magdagresk@onet.pl>
2018-05-12 11:44:31 -0400

    Delete node_modules

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
 var fs = require('fs')
+var colors = require('colors')
 
 fs.readdir('./', function(err, files) {
 	if (err) {
-		console.log(err.message)
+		console.log('error!')
 	} else {
 		console.log(files)
-
-		fs.writeFile('directoryContent.txt', files)
+		fs.writeFile('directoryContent.txt', files, function(err) {
+			if (err) throw err;
+        	console.log('done!'.blue)
+        })
 	}
 })
+
+
+
 
